c3a6332bc1136a3d4c1a09b2207f66c38341b1f3
RianRioos <rianrioslemes@gmail.com>
2024-06-11 21:57:52 -0300

    criando função para sortear

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-let numero  
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
+
+    let sorteados = [];
+    let numero;
+    for (let i = 0; i < quantidade; i++) {
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }
+
+    let resultado = document.getElementById('resultado');
+}
+
+function obterNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
